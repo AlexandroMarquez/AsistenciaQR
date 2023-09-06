acc8b5337781a23bcc0ddcbfa98694f4cfd72e6d
aleksandrovno <al.marquezm@duocuc.cl>
2023-09-06 17:30:55 -0300

    Validacion Login

--- src/app/login-student/login-student.page.ts
+++ src/app/login-student/login-student.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login-student',
@@ -7,9 +14,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginStudentPage implements OnInit {
 
-  constructor() { }
+  formularioLoginStudent: FormGroup;
+
+  constructor(public fb: FormBuilder, 
+    public alertController: AlertController) { 
+
+    this.formularioLoginStudent = this.fb.group({
+      'nombre': new FormControl("", Validators.required),
+      'password' : new FormControl("", Validators.required)
+    })
+
+  }
 
   ngOnInit() {
   }
+
+  async Ingresar() {
+    var f = this.formularioLoginStudent.value;
+    var usuario = JSON.parse(localStorage.getItem('usuario') || 'null');
+
+    if (usuario && usuario.nombre == f.nombre && usuario.password == f.password) {
+      console.log("Ingresado");
+      // Aquí puedes redirigir al usuario a la página de inicio o realizar cualquier otra acción necesaria.
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Datos Incorrectos',
+        message: 'Verifica tus credenciales e intenta nuevamente.',
+        buttons: ['Aceptar']
+      });
+      await alert.present();
+    }
+}
 
 }
--- src/app/registro-estudiante/registro-estudiante.page.ts
+++ src/app/registro-estudiante/registro-estudiante.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro-estudiante',
@@ -7,9 +14,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroEstudiantePage implements OnInit {
 
-  constructor() { }
+  formularioRegistroStudent: FormGroup;
 
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController
+  ) {
+
+    this.formularioRegistroStudent = this.fb.group({
+      'usuario': new FormControl("", Validators.required),
+      'contraseña': new FormControl("", Validators.required),
+      'nombre': new FormControl("", Validators.required),
+      'apellido': new FormControl("", Validators.required),
+      'rut': new FormControl("", Validators.required),
+      'telefono': new FormControl("", Validators.required),
+      'correo': new FormControl("", Validators.required)
+    })
+
+  }
   ngOnInit() {
   }
+
+  async guardar() {
+    var f = this.formularioRegistroStudent.value;
+
+    if (this.formularioRegistroStudent.invalid) {
+      const alert = await this.alertController.create({
+        header: 'Datos incompletos',
+        message: 'Tienes que llenar todos los datos',
+        buttons: ['Aceptar']
+      });
+
+      await alert.present();
+      return;
+  }
+
+  var usuario = {
+      usuario: f.usuario,
+      contraseña: f.contraseña,
+      nombre: f.nombre,
+      apellido: f.apellido,
+      rut: f.rut,
+      telefono: f.telefono,
+      correo: f.correo
+
+  }
+  localStorage.setItem('usuario', JSON.stringify(usuario));
+}
 
 }
