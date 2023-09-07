aeef5948ae081c1a3e01b1fd4d6fdd8178bb98ec
aleksandrovno <al.marquezm@duocuc.cl>
2023-09-07 00:25:44 -0300

    login y registro profesor funcionalidad

--- src/app/login-profesor/login-profesor.page.ts
+++ src/app/login-profesor/login-profesor.page.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router'; // Asegúrate de importar el servicio Router
+
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login-profesor',
@@ -6,10 +15,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-profesor.page.scss'],
 })
 export class LoginProfesorPage implements OnInit {
+  @ViewChild('inputUsuario') inputUsuario!: ElementRef;
+  @ViewChild('inputContraseña') inputContraseña!: ElementRef;
 
-  constructor() { }
+  formularioLoginProfesor: FormGroup = new FormGroup({
+    'user': new FormControl(this.inputUsuario, Validators.required),
+    'password': new FormControl(this.inputContraseña, Validators.required)
+  });
+
+  constructor(
+    public fb: FormBuilder,
+    public alertController: AlertController,
+    private router: Router 
+  ) { }
 
   ngOnInit() {
+    
   }
 
+  ngAfterViewInit() {
+    const inputUsuarioValue = this.inputUsuario.nativeElement.value;
+    const inputContraseñaValue = this.inputContraseña.nativeElement.value;
+
+    this.formularioLoginProfesor.setValue({
+      'user': inputUsuarioValue,
+      'password': inputContraseñaValue
+    });
+  }
+
+  async ingresar() {
+    var varFormularioLogin = this.formularioLoginProfesor.value;
+    var user = JSON.parse(localStorage.getItem('usuario') || '{}');
+    console.log(varFormularioLogin.user);
+    console.log(user.usuario);
+
+    if (user.usuario === varFormularioLogin.user && user.contraseña === varFormularioLogin.password) {
+      console.log("Ingresado");
+      this.router.navigate(['/main-profesor']);
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Datos Incorrectos',
+        message: 'Verifica tus credenciales e intenta nuevamente.',
+        buttons: ['Aceptar']
+      });
+      await alert.present();
+    }
+  }
 }
+
--- src/app/registro-profesor/registro-profesor.page.ts
+++ src/app/registro-profesor/registro-profesor.page.ts
@@ -1,4 +1,26 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-registro-profesor',
+//   templateUrl: './registro-profesor.page.html',
+//   styleUrls: ['./registro-profesor.page.scss'],
+// })
+// export class RegistroProfesorPage implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro-profesor',
@@ -7,9 +29,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroProfesorPage implements OnInit {
 
-  constructor() { }
+  formularioRegistroProfesor: FormGroup;
 
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController
+  ) {
+
+    this.formularioRegistroProfesor = this.fb.group({
+      'usuario': new FormControl("", Validators.required),
+      'contraseña': new FormControl("", Validators.required),
+      'nombre': new FormControl("", Validators.required),
+      'apellido': new FormControl("", Validators.required),
+      'rut': new FormControl("", Validators.required),
+      'telefono': new FormControl("", Validators.required),
+      'correo': new FormControl("", Validators.required)
+    })
+
+  }
   ngOnInit() {
   }
+
+  async guardar() {
+    var f = this.formularioRegistroProfesor.value;
+
+    if (this.formularioRegistroProfesor.invalid) {
+      const alert = await this.alertController.create({
+        header: 'Datos incompletos',
+        message: 'Tienes que llenar todos los datos',
+        buttons: ['Aceptar']
+      });
+
+      await alert.present();
+      return;
+  }
+
+  var usuario = {
+      usuario: f.usuario,
+      contraseña: f.contraseña,
+      nombre: f.nombre,
+      apellido: f.apellido,
+      rut: f.rut,
+      telefono: f.telefono,
+      correo: f.correo
+
+  }
+
+  localStorage.setItem('usuario', JSON.stringify(usuario));
+  const alert = await this.alertController.create({
+    header: 'Guardado',
+    message: 'Datos guardados correctamente',
+    buttons: ['Aceptar']});    
+    await alert.present();
+}
 
 }
