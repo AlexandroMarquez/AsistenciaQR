7dbb09c1f2601e5d5620979688f15cfd294ded09
aleksandrovno <al.marquezm@duocuc.cl>
2023-09-24 21:10:18 -0300

    Animacion recuperar cuenta codeest al ingresar contraseña erronea

--- src/app/recuperar-cuenta-code/recuperar-cuenta-code.page.ts
+++ src/app/recuperar-cuenta-code/recuperar-cuenta-code.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChildren } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router'; // Importa Router desde '@angular/router'
+import { Router } from '@angular/router';
+import type { QueryList } from '@angular/core';
+import { Animation, AnimationController, IonCard } from '@ionic/angular'; // Importa Animation y AnimationController desde '@ionic/angular'
 
 @Component({
   selector: 'app-recuperar-cuenta-code',
@@ -8,28 +10,71 @@ import { Router } from '@angular/router'; // Importa Router desde '@angular/rout
   styleUrls: ['./recuperar-cuenta-code.page.scss'],
 })
 export class RecuperarCuentaCodePage implements OnInit {
-
-  codigoCambiaClave = 'CambiaClave'; 
+  codigoCambiaClave = 'CambiaClave';
 
   formularioRecuperarCuentaCode: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private router: Router) {
+  @ViewChildren(IonCard, { read: ElementRef })
+  cardElements!: QueryList<ElementRef<HTMLIonCardElement>>;
+
+  private animation: Animation | undefined;
+
+  constructor(
+    private animationCtrl: AnimationController,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
     this.formularioRecuperarCuentaCode = this.formBuilder.group({
-      codigo: ['', Validators.required] 
+      codigo: ['', Validators.required],
     });
   }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    // Verificar si hay elementos en cardElements antes de continuar
+    if (this.cardElements && this.cardElements.length > 0) {
+      const firstCard = this.cardElements.first.nativeElement;
+
+      const card = this.animationCtrl
+        .create()
+        .addElement(firstCard)
+        .duration(2000)
+        .beforeStyles({
+          filter: 'invert(75%)',
+        })
+        .beforeClearStyles(['box-shadow'])
+        .afterStyles({
+          'box-shadow': 'rgba(255, 0, 50, 0.4) 0px 4px 16px 6px',
+        })
+        .afterClearStyles(['filter'])
+        .keyframes([
+          { offset: 0, transform: 'scale(1)' },
+          { offset: 0.5, transform: 'scale(1.5)' },
+          { offset: 1, transform: 'scale(1)' },
+        ]);
+
+      this.animation = this.animationCtrl.create().duration(2000).addAnimation([card]);
+    } else {
+      console.error('No se encontraron elementos en cardElements.');
+    }
   }
 
   cambiarClave() {
     const codigoIngresado = this.formularioRecuperarCuentaCode.get('codigo')?.value;
 
     if (codigoIngresado === this.codigoCambiaClave) {
-      console.log('vamos a cambiar la clave');
-      this.router.navigate(['/recuperar-cuenta-code2']); 
+      console.log('Vamos a cambiar la clave');
+      
+      this.router.navigate(['/recuperar-cuenta-code2']);
     } else {
+      if (this.animation) {
+        this.animation.play();
+      } else {
+        console.error('La propiedad "animation" es undefined.');
+      }
       console.log('Código incorrecto');
     }
+  
   }
 }
